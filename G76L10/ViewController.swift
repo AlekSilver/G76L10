c4b07bd16a5904750fc85b0a96e7fd76c970a4f3
AlekSilver <seliverstov.oe@gmail.com>
2020-01-29 13:57:03 +0200

    first commit

---
+++ G76L10/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  G76L10
+//
+//  Created by Oleksandr Seliverstov on 24.01.2020.
+//  Copyright © 2020 Oleksandr Seliverstov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var textView: UITextView!
+    
+    let startPoint: CGPoint = .zero
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        textView.delegate = self
+        progressBar.progress = 0
+        scrollViewDidScroll(textView)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let currentPoint: CGPoint = scrollView.contentOffset
+        let heightOfText = scrollView.contentSize.height
+        let heightOfTextView = scrollView.frame.height        
+        progressBar.progress = (Float(currentPoint.y)/Float(heightOfText-heightOfTextView))
+        self.reloadInputViews()
+    }
+    
+}
+
